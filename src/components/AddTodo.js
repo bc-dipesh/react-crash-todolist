943049dac31bd07494399f7fa807c6cd52dc12d1
bc-dipesh <D.bc8829@student.leedsbeckett.ac.uk>
2020-08-30 15:54:33 +0545

    added prop-types

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class AddTodo extends Component {
   state = {
@@ -37,4 +38,10 @@ class AddTodo extends Component {
   }
 }
 
+// PropTypes
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+};
 export default AddTodo;
